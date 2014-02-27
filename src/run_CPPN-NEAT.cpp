e94deba8e5b81965b8bed9f622519fd2e115a4d8
Warren Eaton S <wes.p160@gmail.com>
2014-02-27 19:59:26 -0300

    -h flag fixed

--- src/run_CPPN-NEAT.cpp
+++ src/run_CPPN-NEAT.cpp
@@ -1,33 +1,3 @@
-/*#include "CPPN-NEAT.hpp"
-#include "genetic_encoding.hpp"
-
-using namespace std;
-using namespace ANN_USM;
-
-#include <ctime>
-
-int main(int argc, char** argv)
-{
-	srand(time(0)); // use current time as seed for random generator
-	
-	if(argc < 2){
-		cerr << "Arguments missing. The relative path to the genetic encoding file ( *.genetic_encoding ) must be defined!" << endl;
-		exit(1);
-	}
-
-	Population poblacion(argv[1]);
-	
-	for (int i = 0; i < GENERATIONS; ++i) poblacion.epoch();
-
-	//char path[] = "./files/result_population";
-	//poblacion.save(path);
-
-	cout << poblacion.champion << endl;
-	cout << "champion: " << poblacion.fitness_champion << endl;
-
-	return 0;
-}*/
-
 
 #include <ctime>
 #include <cstring>
@@ -66,12 +36,12 @@ int main(int argc, char** argv)
 		return -1;	
 	}
 
-	if(argc == 2)
+	/*if(argc == 2)
 	{
 		cerr << "error: missing flag's argument!" << endl;
 		print_usage(argv[0]);
 		return -1;
-	}
+	}*/
 
 	for (int i = 1; i < argc; ++i)
 	{
